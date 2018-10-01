d47d6e5217f2dcb284f9be615b37f88fecb9e613
Lyioh <bidegaray.lionel@mail.com>
2018-10-01 16:44:04 +0200

    done

---
+++ Training Premiere Serie/Exercice 1 et 2.js
@@ -0,0 +1,8 @@
+let message = "Hello world!!!!";
+let nom, prenom, ville;
+
+nom = "BIDEGARAY";
+prenom = "Lionel";
+ville = "Pau";
+
+alert("Nom: " + nom + "\n" + "Prenom: " + prenom + "\n" + "Ville: " + ville);
---
+++ Training Premiere Serie/Exercice 4.js
@@ -0,0 +1,4 @@
+let nom, prenom, ville;
+
+nom = prompt("Enter your name");
+prenom = prompt("Enter your first name");
