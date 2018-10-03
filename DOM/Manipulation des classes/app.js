f74f10b183adc7853595519547d979fc2c2631ec
Lyioh <bidegaray.lionel@mail.com>
2018-10-03 12:29:37 +0200

    add mercredi 12h30

---
+++ DOM/Manipulation des classes/app.js
@@ -0,0 +1,46 @@
+// Methode ancienne il parait ----- oO
+//document.getElementsByClassName("bg-aqua")[0].className = "bg-olive";
+
+//Nouvelle méthode il parait--------
+document.querySelector("body").classList.remove();
+document.querySelector("body").classList.add("bg-olive");
+
+//------------------------
+document.querySelector("#first-paragraph").classList.remove("bg-lime", "gray");
+document.querySelector("#first-paragraph").classList.add("aqua");
+
+//------------- Replace
+let bgSilverInTeal = document.querySelectorAll(".bg-silver");
+for (let i = 0; i < bgSilverInTeal.length; i++) {
+    bgSilverInTeal[i].classList.replace("bg-silver", "bg-teal");
+}
+
+//-----------------------
+let blockquote = document.querySelectorAll("blockquote");
+for (let i = 0; i < blockquote.length; i++) {
+    blockquote[i].classList.add("bg-white");
+}
+
+//-----------------------Selecteur CSS------------------------------------
+document.querySelector("#my-table").classList.add("bg-purple");
+
+//---------------
+let shadow = document.querySelectorAll(".container > p");
+for (let i = 0; i < shadow.length; i++) {
+    shadow[i].classList.add("shadow");
+}
+
+//------------------
+let elemPre = document.querySelectorAll("pre");
+for (let i = 0; i < elemPre.length; i++) {
+    elemPre[i].style.color = "red";
+    elemPre[i].style.backgroundColor = "yellow";
+    elemPre[i].style.borderTop = "3px solid red";
+    elemPre[i].style.borderBottom = "3px solid red";
+}
+
+//----------------
+document.querySelector("h3").innerHTML= "<em>Italic title ! yeah !</em>";
+document.querySelector("h2").innerHTML= "<strong>HTML doens't work !</strong>";
+
+//--------------------------
--- Training Premiere Serie/Exercice 4.js
+++ Training Premiere Serie/Exercice 4.js
@@ -2,3 +2,4 @@ let nom, prenom, ville;
 
 nom = prompt("Enter your name");
 prenom = prompt("Enter your first name");
+ville = prompt("Where do you live?");
